d97cbc77fe513793671be22ec6ff79ddddbf55c8
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-08 20:25:45 +1200

    Add project files.

---
+++ PositiveorNegative/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PositiveorNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter a number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a<0)
+            {
+                Console.WriteLine(a.ToString()+ "  is negative" );
+            }
+            else
+            {
+                Console.WriteLine(a.ToString()+" is positive");
+            }
+            
+        }
+    }
+}
